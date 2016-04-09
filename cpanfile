7a6ce8267d2102e05fa5c9b9c9edc42a1e97f5e2
Fabrice Gabolde <fabrice.gabolde@gmail.com>
2016-04-09 19:28:47 +0200

    Redis.

--- cpanfile
+++ cpanfile
@@ -2,5 +2,6 @@
 
 requires "Dancer2" => "0.163000";
 requires "Dancer2::Plugin::Database" => 0;
+requires "Dancer2::Plugin::Redis" => 0;
 requires "DBD::mysql" => 0;
 requires "YAML";
--- lib/dancing/docker/app.pm
+++ lib/dancing/docker/app.pm
@@ -1,6 +1,7 @@
 package dancing::docker::app;
 use Dancer2;
 use Dancer2::Plugin::Database;
+use Dancer2::Plugin::Redis;
 
 our $VERSION = '0.1';
 
@@ -9,10 +10,14 @@ get '/' => sub {
 };
 
 get '/books' => sub {
+    # not incr because the Redis plugin, frankly, sucks
+    my $visit = redis_get('visits');
+    redis_set('visits', $visit + 1);
     my $sth = database->prepare('SELECT * FROM book');
     $sth->execute;
     my $data = $sth->fetchall_arrayref({});
-    return to_json($data);
+    return to_json({ visit => $visit + 1,
+                     books => $data });
 };
 
 true;
